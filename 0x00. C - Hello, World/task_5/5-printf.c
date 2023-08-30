00e6ad6268f1beddf0fcc65384a5d3602f1629d2
Ami Manye <amimanyeaglago@gmail.com>
2023-08-30 22:04:09 +0000

    program that prints to stdout using printf

---
+++ 0x00. C - Hello, World/task_5/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - using printf to print
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
